9df74c59464dd1741066ca7d301995c0aaf1254a
Davey Morse <davey561@gmail.com>
2024-08-28 00:39:05 -0400

    new edit

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="title">        Morse Tutoring
-        <div className="eightball">❄️</div>
 </div>
         <br></br>
         <div className="bio">
         Davey is very good at inspiring bored kids. 
         <br></br><br></br>
-        Before he raised $1.2m to connect strangers through shared experiences, coded at Apple, wrote a patent,
-        or studied artificial intelligence at Williams, he was a bored kid, too. 
-
-        Davey has the greatest impact on sensitive students who see no point in math, writing, literature, or physics. For them, he reframes these subjects as superpowers... as tools for self-expression.
+        Before he raised $1.2m for his public-benefit company, coded at Apple,
+        studied artificial intelligence, or began tutoring, he was a bored kid, too. 
 <br></br><br></br>
-If you'd like your kid memorize formulas or learn test tricks, another tutor will be better suited.
-<br></br>
-<br></br>
-Davey's current rate is $290 per hour session. <br></br>All sessions are in-person, at your home or a convenient coffee shop.
+        Davey is most helpful for students who see no point in math, writing, literature, or physics. He helps students finds personal connections to these subjects.
+<br></br><br></br>
+Davey's current rate is $290 per hour session. <br></br>All sessions are in-person in NYC, at your home or a coffee shop.
 <br></br><br></br>
 For a consult: <br></br>Text (646) 352-2244, or<br></br>
 Email <a href="mailto:davey@plexus.earth" className='link'>davey@plexus.earth</a>
@@ -29,9 +25,9 @@ Email <a href="mailto:davey@plexus.earth" className='link'>davey@plexus.earth</a
 <br></br>
 
 For more about Davey:<br></br>
-<a href="https://www.linkedin.com/in/davey-morse-25131a182/" className='link'>work</a>&emsp;
-<a href="https://plexus.substack.com/daveys-thoughts" className='link'>writing</a>&emsp;
-<a href="https://plexus.earth" className='link'>plexus</a></div>
+<a href="https://www.linkedin.com/in/davey-morse-25131a182/" className='link'>Career</a>&emsp;
+<a href="https://plexus.substack.com/daveys-thoughts" className='link'>Writing</a>&emsp;
+<a href="https://plexus.earth" className='link'>Plexus</a></div>
 
 <br></br><br></br>
 <i>Davey Morse LLC, all rights reserved.</i>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDK-TemPi6XHZhUF59i4o3j_zTaOqpjSCc",
+  authDomain: "morse-tutoring.firebaseapp.com",
+  projectId: "morse-tutoring",
+  storageBucket: "morse-tutoring.appspot.com",
+  messagingSenderId: "951843546777",
+  appId: "1:951843546777:web:e559c0ba9ee451180e7a08",
+  measurementId: "G-YQ5Q58F4B9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
