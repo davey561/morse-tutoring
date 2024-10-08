927c663b353d6740239ad2dc3a909545162b031c
Davey Morse <davey561@gmail.com>
2024-10-08 01:14:20 -0400

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -13,30 +13,26 @@ function App() {
         <div className="bio">
         Davey is very good at inspiring bored kids. 
         <br></br><br></br>
-        Before he raised $1.2m for his public benefit company, coded at Apple, or studied artificial intelligence, he was a bored kid, too. 
+        Before raising $1.2m for his public benefit company, coding at Apple, or studying AI at Williams, he was a bored kid, too. 
 <br></br><br></br>
         Davey has the greatest impact on students who see no point in math, writing, literature, or physics. 
-He helps students find personal connections to these subjects and do brilliant work.
+He helps students find personal connections to school subjects and excel.
 <br></br><br></br>
-Davey's current rate is $290 per hour session. <br></br>Sessions are in-person in NYC, at your home or a coffee shop.
+Davey's rate is $290 per hour session. <br></br>Sessions are in person in NYC, <br></br>at your home or a coffee shop.
 <br></br><br></br>
 For a consult: <br></br>Text (646) 352-2244, or<br></br>
-Email <a href="mailto:davey@plexus.earth" className='link'>davey@plexus.earth</a>
+Email <a href="mailto:davey@plexus.earth" className='link'>davey@morsetutoring.com</a>
 <br></br>
 <br></br>
 
 For more from Davey:<br></br>
-<a href="https://drive.google.com/drive/folders/1ofJzYy7RnUjDgN83L10LPm9gNsTl5GFf?usp=sharing" className='link'>Lessons</a>&emsp;
-<a href="https://plexus.substack.com/daveys-thoughts" className='link'>Writing</a>&emsp;
+<a href="https://drive.google.com/drive/folders/1ofJzYy7RnUjDgN83L10LPm9gNsTl5GFf?usp=sharing" className='link'>Essays</a>&emsp;
+<a href="https://www.youtube.com/@morse-tutoring/shorts" className='link'>Videos</a>&emsp;
 <a href="https://plexus.earth" className='link'>Plexus</a></div>
 
 <br></br><br></br>
 
-<i>Davey Morse LLC, all rights reserved.</i>
-<br></br>
-{/* <div className="subtitle">        -- --- .-. ... .<br></br>- ..- - -- .-. .. -. --.
-</div> */}
-<div className="subtitle">        -- .. ... -.-. .... .. . ..-. / -- .- -. .- --. . -..
+<div className="subtitle">        -- .. ... -.-. .... .. . ..-. <br></br> -- .- -. .- --. . -..
 </div>
 
 
