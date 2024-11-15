8f3ccd0653e1eb6f7840cffa39199f70b277a778
Davey Morse <davey561@gmail.com>
2024-11-15 00:49:04 -0500

    fine wording

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const bioString = 
+`Davey is very good at inspiring bored kids. 
+
+Before raising $1.2m for his startup, coding at Apple, or studying artificial intelligence, he was a bored kid, too. 
+
+Davey has the greatest impact on students who see no point in writing, reading, or math. He helps students find personal connections to these subjects so they excel.`
 function App() {
+  const formatBio = (bio: string)=> {
+    return <Fragment>{bio.split('/n').map(bioPiece => <div> {bioPiece}<br></br><br></br></div>)}</Fragment>
+  }
   return (
     <div className="App">
       <header className="App-header">
         <div className="title">        Morse Tutoring
 </div>
 
+
+
         <br></br>
         <div className="bio">
-        Davey is very good at inspiring bored kids. 
+          {/* {formatBio(bioString)} */}
+        Davey helps
         <br></br><br></br>
-        Before raising $1.2m for his public benefit company, coding at Apple, or studying AI at Williams, he was a bored kid, too. 
+        Before raising $1.2m for his startup, coding at Apple, or studying Symbolic Systems at Williams, he was a bored kid, too. 
+  <br></br><br></br>
+        Davey has the greatest impact on students who see no point in writing, literature, or math. He helps students find personal connections to their subjects, so they can excel.
 <br></br><br></br>
-        Davey has the greatest impact on students who see no point in math, writing, literature, or physics. 
-He helps students find personal connections to school subjects and excel.
+Davey's rate is $290 per hour-session. <br></br>Sessions are in person in NYC, <br></br>at your home or a coffee shop.
 <br></br><br></br>
-Davey's rate is $290 per hour session. <br></br>Sessions are in person in NYC, <br></br>at your home or a coffee shop.
-<br></br><br></br>
-For a consult: <br></br>Text (646) 352-2244, or<br></br>
-Email <a href="mailto:davey@plexus.earth" className='link'>davey@morsetutoring.com</a>
+For a consult, email Davey: <br></br>
+<a href="mailto:davey@morsetutoring.com?subject=Morse%20Tutoring%20Inquiry" className='link'>davey@morsetutoring.com</a>
 <br></br>
 <br></br>
 
 For more from Davey:<br></br>
-<a href="https://drive.google.com/drive/folders/1ofJzYy7RnUjDgN83L10LPm9gNsTl5GFf?usp=sharing" className='link'>Essays</a>&emsp;
+<a href="https://daveymorse.gumroad.com/l/school-secrets" className='link'>Lessons</a>&emsp;
 <a href="https://www.youtube.com/@morse-tutoring/shorts" className='link'>Videos</a>&emsp;
 <a href="https://plexus.earth" className='link'>Plexus</a></div>
 
